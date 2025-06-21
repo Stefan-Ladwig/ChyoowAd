bc2b83bb8dbef1ad559a39d00872c29059560df5
MrMobius <ladwig.stefan@gmx.de>
2025-06-21 09:06:15 +0200

    added script to get active branch name

---
+++ get_branch_name.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+def get_active_branch_name():
+
+    head_dir = Path(".") / ".git" / "HEAD"
+    with head_dir.open("r") as f: content = f.read().splitlines()
+
+    for line in content:
+        if line[0:4] == "ref:":
+            return line.partition("refs/heads/")[2]
+
+
+if __name__ == "__main__":
+    print(get_active_branch_name())
